0b71284bc8c397fe56922ba65632642f8f44e828
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-10-18 12:36:45 +0330

    storing server action in a seprate file

---
+++ actions/posts.js
@@ -0,0 +1,34 @@
+"use server";
+
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
+export async function createPost(prevState, formData) {
+  // "use server";
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required!");
+  }
+  if (!content || title.trim().length === 0) {
+    errors.push("Title is required!");
+  }
+  if (!image || image.size === 0) {
+    errors.push("image is required!");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  await storePost({
+    imageUrl: "",
+    title,
+    content,
+    userId: 1,
+  });
+  redirect("/feed");
+}
